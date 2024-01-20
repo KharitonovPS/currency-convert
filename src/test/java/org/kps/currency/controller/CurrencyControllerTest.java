8ea6ac933cd28496e8f00f54ec269d159b091a70
KharitonovPS <pasyaoo@gmail.com>
2024-01-21 00:12:20 +0300

    refactor controller unit test

--- src/test/java/org/kps/currency/controller/CurrencyControllerTest.java
+++ src/test/java/org/kps/currency/controller/CurrencyControllerTest.java
@@ -7,6 +7,7 @@ import org.kps.currency.validation.CodeISOValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -24,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(CurrencyController.class)
 @Import({CodeISOValidator.class, CurrencyRepo.class})
+@ComponentScan(basePackages = {"org.kps.currency", "org.kps.currency.controller"})
 class CurrencyControllerTest {
 
     @Autowired
@@ -38,12 +41,16 @@ class CurrencyControllerTest {
     @Test
     void serviceShouldReturnListOfAllQuotes() throws Exception {
         CurrencyEntity entity = new CurrencyEntity(1L, "USD", 840, "Dollar", new BigDecimal("1"), Instant.now());
+        CurrencyEntity entity2 = new CurrencyEntity(2L, "EUR", 111, "EURO", new BigDecimal("2"), Instant.now());
+
+        List<CurrencyEntity> entities = List.of(entity, entity2);
 
         when(repo.findByCharCode("USD")).thenReturn(Optional.of(entity));
+        when(repo.findAll()).thenReturn(entities);
         when(validator.isValid(any(), any())).thenReturn(true);
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/v1/pairs")
+                        .post("/api/v1/pairs")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{ \"quote\" : \"USD\"}"))
@@ -62,7 +69,7 @@ class CurrencyControllerTest {
         when(validator.isValid(any(), any())).thenReturn(true);
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/v1/convert")
+                        .post("/api/v1/convert")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{ \"base\" : \"USD\", \"quote\" :  \"EUR\", \"value\" :  10}"))
