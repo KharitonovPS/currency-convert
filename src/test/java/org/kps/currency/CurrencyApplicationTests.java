3c158bb7c10d1916c74a47aa4a1df653744ffdeb
KharitonovPS <pasyaoo@gmail.com>
2024-01-22 02:00:43 +0300

    refactor CurrencyApplicationTests.java

--- src/test/java/org/kps/currency/CurrencyApplicationTests.java
+++ src/test/java/org/kps/currency/CurrencyApplicationTests.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.kps.currency.client.CurrencyClientImpl;
 import org.kps.currency.client.CurrencyClientTestImpl;
@@ -17,6 +18,7 @@ import org.kps.currency.service.CurrencyConverterService;
 import org.kps.currency.service.SchemaInitService;
 import org.kps.currency.service.SchemaUpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,8 +28,9 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -45,35 +48,34 @@ class CurrencyApplicationTests extends AbstractIntegrationServiceTest {
     @Autowired
     CurrencyClientImpl client;
 
-    CurrencyClientTestImpl clientTest;
-
-    @Autowired
-    SchemaUpdateService updateService;
-
-    @Autowired
-    SchemaInitService initService;
-
     @Autowired
     private EntityManager entityManager;
+    CurrencyClientTestImpl clientTest;
+
+
+    @MockBean
+    SchemaUpdateService updateService;
+
+    @MockBean
+    SchemaInitService initService;
+
+    private static final CurrencyEntity USD = new CurrencyEntity(1L, "USD", 840,
+            "US Dollar", new BigDecimal("1.0952002800"), Instant.now());
+    private static final CurrencyEntity EUR = new CurrencyEntity(2L, "EUR", 978,
+            "Euro", new BigDecimal("0.999999995661"), Instant.now());
 
     @BeforeEach
     void setUp() {
         repo.deleteAll();
-
-        log.info("Preloading " + repo.save(new CurrencyEntity(1L, "USD", 840,
-                "US Dollar", new BigDecimal("1.09520028"), Instant.now())));
-        log.info("Preloading " + repo.save(new CurrencyEntity(2L, "EUR", 978,
-                "Euro", new BigDecimal("0.999999995661"), Instant.now())));
-        log.info("Preloading " + repo.save(new CurrencyEntity(3L, "RUB", 643,
-                "Russian Ruble", new BigDecimal("96.33381334319916"), Instant.now())));
-        log.info("Preloading " + repo.save(new CurrencyEntity(4L, "UAH", 980,
-                "Ukrainian Hryvnia", new BigDecimal("41.39568254086164"), Instant.now())));
-        log.info("Preloading " + repo.save(new CurrencyEntity(5L, "JPY", 392,
-                "Japanese Yen", new BigDecimal("158.55104933532"), Instant.now())));
+        log.info("Preloading " + repo.save(USD));
+        log.info("Preloading " + repo.save(EUR));
 
         if (clientTest == null) {
             clientTest = new CurrencyClientTestImpl(port, 1);
         }
+        doNothing().when(initService).initSchemaOnStartup();
+        doNothing().when(updateService).checkForUpdate();
+
     }
 
     @AfterAll
@@ -82,50 +84,90 @@ class CurrencyApplicationTests extends AbstractIntegrationServiceTest {
     }
 
     @Test
+    @Tag("repository")
     void initializeRepo() {
-        assertEquals(5, repo.count());
+        List<CurrencyEntity> all = repo.findAll();
+        assertAll(
+                () -> assertThat(repo).isNotNull(),
+                () -> assertThat(all).hasSize(2),
+                () -> assertThat(all).usingElementComparatorIgnoringFields("lastModifiedAt", "rate")
+                        .containsExactlyInAnyOrder(USD, EUR)
+        );
     }
 
     @Test
+    @Tag("repository")
     void shouldGetCurrencyByCode() {
         CurrencyEntity entity = repo.findByCharCode("USD").orElseThrow();
 
-        assertEquals(840, entity.getNumCode());
-        assertEquals("US Dollar", entity.getName());
+        assertAll(
+                () -> assertThat(entity).isNotNull(),
+                () -> assertThat(entity.getNumCode()).isEqualTo(840),
+                () -> assertThat(entity.getName()).isEqualTo("US Dollar")
+        );
+
     }
 
     @Test
     @Transactional
+    @Tag("repository")
     void shouldUpdateRateAndTimestamp() {
-        CurrencyEntity entity = repo.findByCharCode("USD").orElseThrow();
-        log.info("Before update: {}", entity.getRate());
-
-        repo.updateRateByCharCode("USD", new BigDecimal(1L), Instant.now());
+        repo.updateRateByCharCode("USD", BigDecimal.ONE, Instant.now());
         entityManager.clear();
-        CurrencyEntity entity2 = repo.findByCharCode("USD").orElseThrow();
-        log.info("After update: {}", entity2.getRate());
-        assertEquals(BigDecimal.ONE, entity2.getRate().stripTrailingZeros());
+        CurrencyEntity currency = repo.findByCharCode("USD").orElseThrow();
+        assertAll(
+                () -> assertThat(currency.getRate().stripTrailingZeros()).isEqualTo("1"),
+                () -> assertThat(currency.getLastModifiedAt()).isGreaterThan(USD.getLastModifiedAt())
+        );
     }
 
     @Test
+    @Tag("controller")
     void shouldGetListOfCurrencyFromController() {
         CurrencyRequestDTOGetListImpl dto = new CurrencyRequestDTOGetListImpl();
         dto.setQuote("USD");
+
         ResponseEntity<List<CurrencyResponseDTO>> listForQuote = clientTest.getListForQuote(dto);
-        log.info(String.valueOf(listForQuote));
-        assertEquals(5, Objects.requireNonNull(listForQuote.getBody()).size());
+
+        assertAll(
+                () -> assertThat(listForQuote).isNotNull(),
+                () -> assertThat(listForQuote.getBody()).isNotEmpty(),
+                () -> assertThat(listForQuote.getBody()).hasSize(2),
+                () -> assertThat(listForQuote.getStatusCode().toString()).isEqualTo("200 OK")
+        );
     }
 
     @Test
+    @Tag("controller")
     void shouldConvertCurrency() {
         CurrencyRequestDTOConvertImpl dto = new CurrencyRequestDTOConvertImpl();
-        dto.setQuote("RUB");
+        dto.setQuote("EUR");
         dto.setBase("USD");
         dto.setValue(1000L);
 
         ResponseEntity<String> value = clientTest.convert(dto);
-        log.info(value.toString());
-        assertTrue(Objects.requireNonNull(value.getBody()).contains("87960"));
+
+        assertAll(
+                () -> assertThat(value).isNotNull(),
+                () -> assertThat(value.getStatusCode().toString()).isEqualTo("200 OK"),
+                () -> assertThat(value.getBody()).isEqualTo("913.07")
+        );
     }
 
+    @Test
+    @Tag("controller")
+    void throwExceptionIfISOCodeIsNotValid() {
+        CurrencyRequestDTOGetListImpl dto = new CurrencyRequestDTOGetListImpl();
+        dto.setQuote("ZZZ");
+
+        assertAll(
+                () -> {
+                    RuntimeException validationEx = assertThrows(
+                            RuntimeException.class, () -> clientTest.getListForQuote(dto)
+                    );
+                    assertTrue(validationEx.getMessage().contains("The input must be CodeISO"));
+                },
+                () -> assertThrows(RuntimeException.class, () -> clientTest.getListForQuote(null))
+        );
+    }
 }
